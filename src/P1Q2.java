509621a47e78fed29c0fc7a0509aeffb63fcf3f9
LIN123 <LIN123@users.noreply.github.com>
2019-09-23 15:11:27 -0400

    1

--- src/P1Q2.java
+++ src/P1Q2.java
@@ -15,10 +15,13 @@ public class P1Q2 {
         int index = 0;
         char ch1 = input.charAt(0);
         if(ch1 != '(') {
-            printOutput(index,input);
+            errorOutput(index,input);
             
         }else {index = index+1;} 
-        
+        ch1 = input.charAt(index);
+        if(ch1 !=) {
+            
+        }
         while( index< length) {
 
         
@@ -31,7 +34,7 @@ public class P1Q2 {
         
     }
     
-    public static void printOutput(int index, String input) {
+    public static void errorOutput(int index, String input) {
         System.out.println(input);
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < index-1; i++) {
@@ -39,6 +42,12 @@ public class P1Q2 {
          }
         result.append("^");
         System.out.println(result);
+    }
+    
+    
+    
+    public void rightoutput(String s) {
+        ////////`````
     }
     
     
