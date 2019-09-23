6781676c8db5b306550b7035ee5838d059571316
LIN123 <LIN123@users.noreply.github.com>
2019-09-23 19:00:54 -0400

    2

--- src/P1Q2.java
+++ src/P1Q2.java
@@ -3,36 +3,53 @@ public class P1Q2 {
 
     public static void main(String[] args) {
         
-        String result = checkpasher("stack");
+        String result = checkpasher("(2222");
         System.out.println(result);
         
  
         }
 
     public static String checkpasher(String input) {
-        
         int length = input.length();
         int index = 0;
         char ch1 = input.charAt(0);
+        System.out.println(ch1);
         if(ch1 != '(') {
             errorOutput(index,input);
-            
-        }else {index = index+1;} 
-        ch1 = input.charAt(index);
-        if(ch1 !=) {
-            
         }
-        while( index< length) {
+        else {index = index+1;} 
+        
+        if(Character.isDigit(input.charAt(index))) {
+            errorOutput(index,input);
+        }
+        else {
+            index = index+1;
+            while(Character.isDigit(input.charAt(index))) {
+                index = index+1;
+            }
+               System.out.println(index);
+        }
 
-        
-        
-        }
        
         return null;
         
         
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     public static void errorOutput(int index, String input) {
         System.out.println(input);
