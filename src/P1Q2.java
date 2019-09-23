f2bfca3b4e9a311d30fe4ac8b552e99de7466c77
LIN123 <LIN123@users.noreply.github.com>
2019-09-23 19:57:56 -0400

    check char

--- src/P1Q2.java
+++ src/P1Q2.java
@@ -2,77 +2,111 @@
 public class P1Q2 {
 
     public static void main(String[] args) {
-        
-        String result = checkpasher("(2222");
-        System.out.println(result);
-        
- 
-        }
+        checkpasher("(22222->sharescc");
+    }
 
-    public static String checkpasher(String input) {
+
+    public static void checkpasher(String input) {
         int length = input.length();
         int index = 0;
         char ch1 = input.charAt(0);
-        System.out.println(ch1);
-        if(ch1 != '(') {
-            errorOutput(index,input);
+        if (ch1 != '(') {
+            errorOutput(index, input);
         }
-        else {index = index+1;} 
-        
-        if(Character.isDigit(input.charAt(index))) {
-            errorOutput(index,input);
+        index = checkTrade(index, length, input);
+        index = checkChar('-', index, length, input);
+        index = checkChar('>', index, length, input);
+       // index = checkStock_Symbol(index, length, input);
+        index = checkChar('s', index, length, input);
+        index = checkChar('h', index, length, input);
+        index = checkChar('a', index, length, input);
+        index = checkChar('r', index, length, input);
+        index = checkChar('e', index, length, input);
+        index = checkChar('s', index, length, input);
+        System.out.println(index);
+        System.out.println("Come here already");
+
+    }
+
+
+    private static int checkChar(char c, int index, int length, String input) {
+        char ch1 = input.charAt(index);
+        if (ch1 != c) {
+            errorOutput(index, input);
         }
         else {
-            index = index+1;
-            while(Character.isDigit(input.charAt(index))) {
-                index = index+1;
+            index = index + 1;
+            if (index == length) {
+                errorOutput(index, input);
             }
-               System.out.println(index);
         }
-
-       
-        return null;
-        
-        
-        
+        return index;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    private static int checkTrade(int index, int length, String input) {
+        index = index + 1;
+        if (index == length) {
+            errorOutput(index, input);
+        }
+        if (Character.isDigit(input.charAt(index))) {
+            index = index + 1;
+            if (index == length) {
+                errorOutput(index, input);
+            }
+            while (Character.isDigit(input.charAt(index))) {
+                index = index + 1;
+                if (index == length) {
+                    errorOutput(index, input);
+                }
+            }
+        }
+        else {
+            errorOutput(index, input);
+
+        }
+        return index;
+    }
+
     public static void errorOutput(int index, String input) {
         System.out.println(input);
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < index-1; i++) {
+        for (int i = 0; i < index; i++) {
             result.append(" ");
-         }
+        }
         result.append("^");
         System.out.println(result);
+        System.exit(0);
     }
-    
-    
+
+//  private static int checkStock_Symbol(int index, int length, String input) {
+//
+//      char ch1 = input.charAt(index);
+//      if (ch1 != '-') {
+//          errorOutput(index, input);
+//      }
+//      else {
+//          index = index + 1;
+//          if (index == length) {
+//              errorOutput(index, input);
+//          }
+//          ch1 = input.charAt(index);
+//          if (ch1 != '>') {
+//              errorOutput(index, input);
+//          }
+//          else {
+//              index = index + 1;
+//              if (index == length) {
+//                  errorOutput(index, input);
+//              }
+//              return index;
+//          }
+//      }
+//      return index;
+//  }
     
     public void rightoutput(String s) {
-        ////////`````
+        //////// `````
     }
-    
-    
-    
-    
-    
-    
-    
-    
 
 }
