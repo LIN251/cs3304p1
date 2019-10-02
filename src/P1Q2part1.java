fcdce39f1638f50e7d50584b02cab86af2765d44
LIN123 <LIN123@users.noreply.github.com>
2019-10-02 00:36:45 -0400

    1

--- src/P1Q2part1.java
+++ src/P1Q2part1.java
@@ -16,12 +16,7 @@ public class P1Q2part1 {
             "(  99800     GOOGL   shares    buy at max    5555)    "
                 + "for account                \"example\"";
         newRequest.checkpasher(aString1, index);
-        
-        //1.
-        //需要fix 输入两条request
-        //因为完事一条request直接就exit了，所以不会运行第二条指令
-        //2.
-        //cancel 的正确output
+
     }
 
 }
