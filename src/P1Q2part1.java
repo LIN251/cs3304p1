3f2ec701798b754a3d11343d11d8d6e999027676
LIN123 <LIN123@users.noreply.github.com>
2019-10-06 16:40:11 -0400

    nothing change

--- src/P1Q2part1.java
+++ src/P1Q2part1.java
@@ -5,24 +5,24 @@ public class P1Q2part1 {
         addRequest newRequest = new addRequest();
         int index = 0;
 
-//        // correct example with more spaces
-//        // more stacks
-//        String aString =
-//            "(  99800       GOOGL     shares        buy at max           5555,  "
-//                + "99800 GOOGL shares sell at min 10, "
-//                + "8888AAPLsharesbuy at max999  )    "
-//                + "for account                \"linzhang_zhouxiaolin112233\"";
-//        newRequest.checkpasher(aString, index);
-//
-//        // correct example with one space
-//        String aString1 =
-//            "( 99800 GOOGL shares buy at max 5555)for account \"example\"";
-//        newRequest.checkpasher(aString1, index);
-//
-//        // correct example with no space
-//        String aString3 =
-//            "(99800AMZNsharesbuy at max5555)for account\"testexmaple___a\"";
-//        newRequest.checkpasher(aString3, index);
+        // correct example with more spaces
+        // more stacks
+        String aString =
+            "(  99800       GOOGL     shares        buy at max           5555,  "
+                + "99800 GOOGL shares sell at min 10, "
+                + "8888AAPLsharesbuy at max999  )    "
+                + "for account                \"linzhang_zhouxiaolin112233\"";
+        newRequest.checkpasher(aString, index);
+
+        // correct example with one space
+        String aString1 =
+            "( 99800 GOOGL shares buy at max 5555)for account \"example\"";
+        newRequest.checkpasher(aString1, index);
+
+        // correct example with no space
+        String aString3 =
+            "(99800AMZNsharesbuy at max5555)for account\"testexmaple___a\"";
+        newRequest.checkpasher(aString3, index);
 
         // correct example with more spaces
         // more stacks
